bdf83d3016af272c4e03774955a26dd85d868d49
Srividhya G <srividhya.guru07@gmail.com>
2026-06-19 11:39:05 +0400

    Handle Jwt Exceptions

--- auth-service/src/main/java/com/hireflow/auth/security/jwt/JwtAuthenticationFilter.java
+++ auth-service/src/main/java/com/hireflow/auth/security/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.hireflow.auth.security.jwt;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,15 +26,30 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            String token = authorizationHeader.substring(7);
-            JwtClaims jwtClaims = jwtService.extractJwtClaims(token);
-            Authentication authentication = new UsernamePasswordAuthenticationToken(jwtClaims, null,
-                    List.of(new SimpleGrantedAuthority("ROLE_" + jwtClaims.role())));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try{
+            String authorizationHeader = request.getHeader("Authorization");
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                String token = authorizationHeader.substring(7);
+                JwtClaims jwtClaims = jwtService.extractJwtClaims(token);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(jwtClaims, null,
+                        List.of(new SimpleGrantedAuthority("ROLE_" + jwtClaims.role())));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException e){
+            SecurityContextHolder.clearContext();
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"Expired JWT token\"}");
+
+        } catch (JwtException e) {
+            SecurityContextHolder.clearContext();
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"Invalid JWT token\"}");
         }
-        filterChain.doFilter(request, response);
+
     }
 }
--- auth-service/src/main/java/com/hireflow/auth/security/jwt/JwtService.java
+++ auth-service/src/main/java/com/hireflow/auth/security/jwt/JwtService.java
@@ -4,7 +4,6 @@ package com.hireflow.auth.security.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
